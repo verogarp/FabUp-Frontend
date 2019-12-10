859d992e3cbe090fcd3b612783323c98599d2ccc
Verónica García Pulido <verogarp@gmail.com>
2019-12-10 15:57:40 +0000

    Finishes create ad

--- pages/new-ad.vue
+++ pages/new-ad.vue
@@ -11,95 +11,149 @@
           <h1 class="fabup-title-font display-1">Create ad</h1>
         </v-col>
       </v-row>
+
       <v-row align="center" justify="center" class="mx-0">
         <v-col cols="12">
-          <v-text-field label="Title" outlined></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row align="center" justify="center" class="mx-0">
-        <v-col cols="12">
-          <v-text-field label="Price" outlined></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row align="center" justify="center" class="mx-0">
-        <v-col cols="12">
-          <v-text-field label="Location" outlined></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row align="center" justify="center" class="mx-0">
-        <v-col cols="12">
-          <v-text-field label="Description" outlined></v-text-field>
+          <v-text-field
+            v-model="title"
+            label="Title"
+            outlined
+            validate-on-blur
+            :rules="[rules.required]"
+          ></v-text-field>
         </v-col>
       </v-row>
 
       <v-row align="center" justify="center" class="mx-0">
         <v-col cols="12">
-          <input
-            id="file"
-            ref="file"
-            name="file"
-            type="file"
-            multiple
-            @change="handleFileUpload()"
-          />
+          <v-text-field
+            v-model="price"
+            label="Price"
+            outlined
+            validate-on-blur
+            :rules="[rules.required]"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" justify="center" class="mx-0">
+        <v-col cols="12">
+          <v-text-field
+            v-model="location"
+            label="Location"
+            outlined
+            validate-on-blur
+            :rules="[rules.required]"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" justify="center" class="mx-0">
+        <v-col cols="12">
+          <v-text-field
+            v-model="description"
+            label="Description"
+            outlined
+            validate-on-blur
+            :rules="[rules.required]"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" justify="center" class="mx-0">
+        <v-col cols="12">
+          <v-overflow-btn
+            v-model="selectedCategory"
+            class="my-2"
+            :items="categories"
+            item-text="category"
+            item-value="_id"
+            label="Select Category"
+            target="#dropdown-example"
+          ></v-overflow-btn>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" justify="center" class="mx-0">
+        <v-col cols="12">
           <label for="file">
-            <p large width="100%" tile color="primary">
-              <v-alert type="warning" icon="mdi-image-plus" color="#f9a825"
-                >CHOOSE PICTURES</v-alert
-              >
-            </p>
+            <v-alert type="warning" icon="mdi-image-plus" color="#f9a825" dense
+              >UPLOAD PHOTO</v-alert
+            >
           </label>
         </v-col>
       </v-row>
 
-      <!-- <v-row>
-        <v-col cols="12">
-          <v-overflow-btn
-            class="my-2"
-            :items="dropdown_font"
-            label="Overflow Btn"
-            target="#dropdown-example"
-          ></v-overflow-btn>
-        </v-col>
-      </v-row>-->
-
       <v-row align="center" justify="center" class="mx-0">
         <v-col cols="12">
-          <v-btn large width="100%" tile color="primary">CREATE</v-btn>
+          <v-btn large width="100%" tile color="primary" @click="postAd"
+            >POST AD</v-btn
+          >
         </v-col>
       </v-row>
     </v-container>
+    <input
+      id="file"
+      ref="file"
+      name="file"
+      type="file"
+      @change="handleFileUpload"
+    />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from '~/plugins/axios'
 export default {
-  // https://serversideup.net/uploading-files-vuejs-axios/
   data() {
     return {
-      file: ''
+      file: '',
+      title: '',
+      price: '',
+      location: '',
+      description: '',
+      rules: {
+        required: v => !!v || 'Item is required',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        }
+      },
+      categories: [],
+      selectedCategory: ''
     }
   },
+  mounted() {
+    this.getAllCategories()
+  },
   methods: {
-    submitFile() {
-      let formData = new FormData()
-      formData.append('file', this.file)
-      axios
-        .post('https://jsonplaceholder.typicode.com/photos', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        })
-        .then(function() {
-          console.log('SUCCESS!!')
-        })
-        .catch(function() {
-          console.log('FAILURE!!')
-        })
-    },
     handleFileUpload() {
-      this.file = this.$refs.file.files[0]
+      // When selecting images, convert to Base64 string
+      const reader = new FileReader() // A FileReader is used to convert into different formats
+      reader.onloadend = () => {
+        this.file = reader.result // If it finishes loading, we push the result into the `files` array
+      }
+      reader.onerror = Promise.reject // If not, we throw an error
+      reader.readAsDataURL(this.$refs.file.files[0]) //  We tell the reader to convert the image into the base64 string
+    },
+    async postAd() {
+      try {
+        const ad = {
+          title: this.title,
+          price: this.price,
+          location: this.location,
+          description: this.description,
+          image: this.file,
+          category: this.selectedCategory
+        }
+        await axios.post('ads/create', ad)
+      } catch (e) {
+        console.error(e.response.data.message)
+      }
+    },
+    async getAllCategories() {
+      const response = await axios.get('/categories')
+      this.categories = response.data
     }
   }
 }
